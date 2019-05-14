d5a137f9fa191783c710620d27740cb83ae68815
DESKTOP-0OGAHJK\felix <felixnilsson100@gmail.com>
2019-05-14 12:09:30 +0200

    small changes to the fence class

--- PlanetEditor/src/Geometry/Fence.cpp
+++ PlanetEditor/src/Geometry/Fence.cpp
@@ -4,7 +4,7 @@
 #include "DX12/DX12Renderer.h"
 
 
-Fence::Fence(DX12Renderer* renderer, float x, float z) {
+Fence::Fence(DX12Renderer* renderer, XMFLOAT3 p1, XMFLOAT3 p2, XMFLOAT3 p3, XMFLOAT3 p4) {
 	int nrVertices = 12;
 	int nrIndices = 24;
 	float size = 30;
@@ -16,26 +16,38 @@ Fence::Fence(DX12Renderer* renderer, float x, float z) {
 	vertices = new Vertex[nrVertices];
 	indices = new unsigned int[nrIndices];
 
-	vertices[0] =  { XMFLOAT3{x + -size,     0.1f,z + -size}      , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
-	vertices[1] =  { XMFLOAT3{x + -size + 1, 0.1f,z + -size}  , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
-	vertices[2] =  { XMFLOAT3{x + -size,     0.1f,z + -size + 1}  , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
-	vertices[3] =  { XMFLOAT3{x + size - 1,  0.1f,z + -size}   , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
-	vertices[4] =  { XMFLOAT3{x + size,      0.1f,z + -size}       , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
-	vertices[5] =  { XMFLOAT3{x + size,      0.1f,z + -size + 1}   , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
-	vertices[6] =  { XMFLOAT3{x + size,      0.1f,z + size - 1}    , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
-	vertices[7] =  { XMFLOAT3{x + size,      0.1f,z + size}        , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
-	vertices[8] =  { XMFLOAT3{x + size - 1,  0.1f,z + size}    , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
-	vertices[9] =  { XMFLOAT3{x + -size + 1, 0.1f,z + size}   , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
-	vertices[10] = { XMFLOAT3{x + -size,     0.1f,z + size}      , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
-	vertices[11] = { XMFLOAT3{x + -size,     0.1f,z + size - 1}  , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	//vertices[0] =  { XMFLOAT3{x + -size,     0.1f,z + -size}      , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	//vertices[1] =  { XMFLOAT3{x + -size + 1, 0.1f,z + -size}  , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	//vertices[2] =  { XMFLOAT3{x + -size,     0.1f,z + -size + 1}  , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	//vertices[3] =  { XMFLOAT3{x + size - 1,  0.1f,z + -size}   , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	//vertices[4] =  { XMFLOAT3{x + size,      0.1f,z + -size}       , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	//vertices[5] =  { XMFLOAT3{x + size,      0.1f,z + -size + 1}   , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	//vertices[6] =  { XMFLOAT3{x + size,      0.1f,z + size - 1}    , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	//vertices[7] =  { XMFLOAT3{x + size,      0.1f,z + size}        , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	//vertices[8] =  { XMFLOAT3{x + size - 1,  0.1f,z + size}    , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	//vertices[9] =  { XMFLOAT3{x + -size + 1, 0.1f,z + size}   , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	//vertices[10] = { XMFLOAT3{x + -size,     0.1f,z + size}      , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	//vertices[11] = { XMFLOAT3{x + -size,     0.1f,z + size - 1}  , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	vertices[0] = { p1      , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	vertices[1] = { XMFLOAT3{p1.x + 1, p1.y, p1.z}  , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	vertices[2] = { XMFLOAT3{p1.x, p1.y, p1.z + 1}  , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	vertices[3] = { XMFLOAT3{p2.x - 1, p2.y, p2.z}   , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	vertices[4] = { p2   , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	vertices[5] = { XMFLOAT3{p2.x, p2.y, p2.z + 1}   , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	vertices[6] = { XMFLOAT3{p3.x, p3.y, p3.z - 1}    , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	vertices[7] = { p3        , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	vertices[8] = { XMFLOAT3{p3.x - 1, p3.y, p3.z}    , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	vertices[9] = { XMFLOAT3{p4.x - 1, p4.y, p4.z}   , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	vertices[10] = { p4      , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
+	vertices[11] = { XMFLOAT3{p4.x, p4.y, p4.z -1}  , XMFLOAT3{0.f, 1.f, 0.f}, XMFLOAT2{0,0} };
 
 	//bottom
 	indices[0] = 0;
-	indices[1] = 5;
-	indices[2] = 4;
+	indices[1] = 4;
+	indices[2] = 5;
 	indices[3] = 5;
-	indices[4] = 0;
-	indices[5] = 2;
+	indices[4] = 2;
+	indices[5] = 0;
 
 	//right
 	indices[6] = 4;
@@ -47,16 +59,16 @@ Fence::Fence(DX12Renderer* renderer, float x, float z) {
 
 	//top
 	indices[12] = 6;
-	indices[13] = 10;
-	indices[14] = 7;
+	indices[13] = 7;
+	indices[14] = 10;
 	indices[15] = 10;
-	indices[16] = 6;
-	indices[17] = 11;
+	indices[16] = 11;
+	indices[17] = 6;
 
 	//left
 	indices[18] = 9;
-	indices[19] = 0;
-	indices[20] = 10;
+	indices[19] = 10;
+	indices[20] = 0;
 	indices[21] = 0;
 	indices[22] = 9;
 	indices[23] = 1;
